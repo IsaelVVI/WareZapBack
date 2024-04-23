b980550415246e154a1f98621b8dd27ab538bec6
Isael Gomes <otakustudio188@gmail.com>
2024-04-23 18:32:40 -0300

    add schema for example in GoOrm and add new logger example for console

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"github.com/IsaelVVI/warezapback.git/config"
 	"github.com/IsaelVVI/warezapback.git/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+
+	logger = config.GetLogger("main")
+
+	// Initialize Configs app
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config initialize error: %v", err)
+		return
+	}
 
 	// Inicializando o package Router e API
 	router.Initialize()
