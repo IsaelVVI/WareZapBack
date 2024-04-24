6b666937a07b132bc8e95c3228e223aaf248e47f
Isael Gomes <otakustudio188@gmail.com>
2024-04-23 22:02:52 -0300

    add logic in a example for route CREATE test

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,14 +1,33 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Opening struct {
-	Id       uint `gorm:"primaryKey;autoIncrement"`
-	Role     string
-	Company  string
-	Location string
-	Remote   bool
-	Link     string
-	Salary   int64
-	gorm.Model
+	Id        uint `gorm:"primaryKey;autoIncrement"`
+	Role      string
+	Company   string
+	Location  string
+	Remote    bool
+	Link      string
+	Salary    int64
+	CreatedAt time.Time `gorm:autoCreateTime`
+	UpdatedAt time.Time `gorm:autoUpdateTime`
+	DeletedAt gorm.DeletedAt
+}
+
+type OpeningResponse struct {
+	Id        uint      `json:"id"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at,omitempty"`
 }
